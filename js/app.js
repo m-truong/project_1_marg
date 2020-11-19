1996627dfb49184ac8d3e3fd238bc407a30587ef
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-19 16:18:43 -0500

    reached mvp

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// ============================================================= // 
 /* ======================
 CACHED DOM NOTES
 =========================*/
@@ -13,12 +12,16 @@ const $selectDuelist = $(".selectDuelist")
 const $gameContainer = $('.gameContainer')
 
 // ======= Monster Card Img <div> ======= //
-const $playerMonsterCard = $('.playerMonsterCard')
-const $computerMonsterCard = $('.computerMonsterCard')
+const $player1MonsterCard = $('.player1MonsterCard')
+const $player2MonsterCard = $('.player2MonsterCard')
 
 // ======= Life Points <div> ======= // 
-const $playerLifePoints = $('.playerLifePoints')
-const $computerLifePoints = $('.computerLifePoints')
+const $player1LifePoints = $('.player1LifePoints')
+const $player2LifePoints = $('.player2LifePoints')
+
+// Player 1 and Player 2 Attack Buttons // 
+const $atkBtn1 = $('.atkBtn1')
+const $atkBtn2 = $('.atkBtn2')
 
 /* ======================
 GLOBAL VARS
@@ -53,121 +56,89 @@ $duelBtn.click(removeIntroShowCarousel);
 $selectDuelist.click(removeDuelistCarouselshowGameContainer)
 
 /* =============================
-CLASSES
+GAMESTATE CLASS
 ============================= */
-class Player {
-    constructor() {
-        this.lifePoints = 2000;
-        this.monsterCard = null;
-    }
-}
+
 /**
- * The GameState class contains two properties for the player object and computer object.
+ * The GameState class contains a player1 object property, player2 object property,
+ * and two methods that are bound to object instances of the class to allow the click event-listeners to work.
  */
 class GameState {
     constructor() {
-        this.state = true;
-        this.iterable = 0;
-        this.player = new Player();
-        this.computer = new Player();
-
-        // Gave Function to the button, so "this" refers to the button, NOT the class-object instance 
-        // Always going to refer to "this" instance of object // only for refering to property of "this" object
-        // if invoking with an "event-listener", then use bind() when inside async function
+        this.player1 = {
+            lifePoints: 2000,
+            monsterCard: null,
+        }
+        this.player2 = {
+            lifePoints: 2000,
+            monsterCard: null,
+        }
         this.singleBattlePhase = this.singleBattlePhase.bind(this);
         this.initialTurn = this.initialTurn.bind(this);
-        // bind just in case 
     }
     // ================= Appending monsterCard to the page =========================== // 
     displayMonsterCard(card, node) {
-        node.html(`<img src="${card.cardImg}" alt="card">`);
+        const tempImg = node.attr("src");
+        node.attr("src", `${card.cardImg}`);
+        if (tempImg !== card.cardImg) {
+            node.toggleClass('animate__animated animate__jello');
+            setTimeout(() => {
+                node.toggleClass('animate__animated animate__jello');
+            }, 500);
+        }
     }
     // ======= Get Random Monster Card ======= // 
     getRandMonstCard(array) {
         return array[Math.floor(Math.random() * array.length)];
     }
     // ======= Updates HTML to Display Life Points ============= //
-    updateLifePoints(player, node, num) {
-        node.html(`
-        <div class="col card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${player.lifePoints}</h5>
-                <a href="#" class="drawBtn${num} btn btn-secondary">Draw</a>
-                <a href="#" class="atkBtn${num} btn btn-success">Attack</a>
-            </div>
-        </div>
-        `);
-        console.log(node);
+    updateLifePoints(player, node) {
+        node.html(`${player.lifePoints}`);
+
     }
     initialTurn() {
-        this.player.monsterCard = this.getRandMonstCard(monsterList); 
-        this.computer.monsterCard = this.getRandMonstCard(monsterList);
-
-        this.displayMonsterCard(this.player.monsterCard, $playerMonsterCard);
-        this.displayMonsterCard(this.computer.monsterCard, $computerMonsterCard);
-        this.updateLifePoints(this.player, $playerLifePoints, 1);
-        this.updateLifePoints(this.computer, $computerLifePoints, 2);
+        this.player1.monsterCard = this.getRandMonstCard(monsterList);
+        this.player2.monsterCard = this.getRandMonstCard(monsterList);
+        this.displayMonsterCard(this.player1.monsterCard, $player1MonsterCard);
+        this.displayMonsterCard(this.player2.monsterCard, $player2MonsterCard);
+        this.updateLifePoints(this.player1, $player1LifePoints);
+        this.updateLifePoints(this.player2, $player2LifePoints);
     }
     sentToGraveyardDrawNewCard(player, node) {
         player.monsterCard = this.getRandMonstCard(monsterList);
         this.displayMonsterCard(player.monsterCard, node)
     }
-
-    singleBattlePhase(evt) {
-        evt.preventDefault();
-        console.log("Single battle-phase has occurred!");
-
-        if (this.player.monsterCard.atk > this.computer.monsterCard.atk) {
-
+    singleBattlePhase() {
+        if (this.player1.monsterCard.atk > this.player2.monsterCard.atk) {
             alert(`After initiating the Attack Phase, Player 1's Monster Card's Attack Points are greater than Player 2's Monster Card's Attack Points!`)
-
-            this.computer.lifePoints -= (this.player.monsterCard.atk - this.computer.monsterCard.atk)
-
-            alert(`The attack did ${(this.player.monsterCard.atk - this.computer.monsterCard.atk)} damage to Player 2's Life Points!`);
-
-            this.updateLifePoints(this.computer, $computerLifePoints);
-
-            alert(`${this.computer.monsterCard.name} has been sent to the Graveyard!`);
-
-            this.sentToGraveyardDrawNewCard(this.computer, $computerMonsterCard)
-
-        } else if (this.player.monsterCard.atk < this.computer.monsterCard.atk) {
+            this.player2.lifePoints -= (this.player1.monsterCard.atk - this.player2.monsterCard.atk)
+            alert(`The attack did ${(this.player1.monsterCard.atk - this.player2.monsterCard.atk)} damage to Player 2's Life Points!`);
+            this.updateLifePoints(this.player2, $player2LifePoints);
+            alert(`${this.player2.monsterCard.name} has been sent to the Graveyard!`);
+            this.sentToGraveyardDrawNewCard(this.player2, $player2MonsterCard)
+        } else if (this.player1.monsterCard.atk < this.player2.monsterCard.atk) {
             alert(`After initiating the Attack Phase, Player 2's Monster Card's Attack Points are greater than Player 1's Monster Card's Attack Points!`)
-
-            this.player.lifePoints -= (this.computer.monsterCard.atk - this.player.monsterCard.atk)
-
-            alert(`The attack did ${(this.computer.monsterCard.atk - this.player.monsterCard.atk)} damage to Player 1's Life Points!`);
-
-            this.updateLifePoints(this.player, $playerLifePoints);
-
-            alert(`${this.player.monsterCard.name} has been sent to the Graveyard!`);
-
-            this.sentToGraveyardDrawNewCard(this.player, $playerMonsterCard);
-
-        } else if (this.player.monsterCard.atk === this.computer.monsterCard.atk) {
+            this.player1.lifePoints -= (this.player2.monsterCard.atk - this.player1.monsterCard.atk)
+            alert(`The attack did ${(this.player2.monsterCard.atk - this.player1.monsterCard.atk)} damage to Player 1's Life Points!`);
+            this.updateLifePoints(this.player1, $player1LifePoints);
+            alert(`${this.player1.monsterCard.name} has been sent to the Graveyard!`);
+            this.sentToGraveyardDrawNewCard(this.player1, $player1MonsterCard);
+        } else if (this.player1.monsterCard.atk === this.player2.monsterCard.atk) {
             alert(`No damage was done to either players' Life Points, but both players' Monster Cards are sent to the Graveyard!`);
-            // alert (`Both ${this.player.monsterCard.name} and ${this.computer.monsterCard.name} have been sent to the Graveyard!`);
-
-            alert(`${this.computer.monsterCard.name} has been sent to the Graveyard!`);
-            this.sentToGraveyardDrawNewCard(this.computer, $computerMonsterCard);
-
+            alert(`${this.player2.monsterCard.name} has been sent to the Graveyard!`);
+            this.sentToGraveyardDrawNewCard(this.player2, $player2MonsterCard);
             alert(`${this.player.monsterCard.name} has been sent to the Graveyard!`);
-            this.sentToGraveyardDrawNewCard(this.player, $playerMonsterCard);
-
-
+            this.sentToGraveyardDrawNewCard(this.player1, $playerMonsterCard);
         }
-
         this.checkWinState();
     }
-    checkWinState() { // you do have negative numbers in YuGiOh
-        if (this.player.lifePoints > 0 && this.computer.lifePoints <= 0) {
+    checkWinState() {
+        if (this.player1.lifePoints > 0 && this.player2.lifePoints <= 0) {
             alert("Player 1 has won! Thank you for playing!");
             location.reload();
-            // this.state = false;
-        } else if (this.player.lifePoints <= 0 && this.computer.lifePoints > 0) {
+        } else if (this.player1.lifePoints <= 0 && this.player2.lifePoints > 0) {
             alert("Player 2 has won! Thank you for playing!");
             location.reload();
-            // this.state = false;
         }
     }
 }
@@ -175,6 +146,7 @@ class GameState {
 /* =============================
 Yu-Gi-Oh API Database
 ============================= */
+
 // ====== Global GameState ====== //
 const game1 = new GameState()
 
@@ -195,14 +167,9 @@ const yugioh = async () => {
 yugioh().then(
 
     () => {
-        console.log('Execute all JavaScript inside this .then() method');
-        console.log('inside', cardData);
-
         cardData.forEach((currMonstCard) => {
             const monsterCardObj = {
                 name: currMonstCard.name,
-                // race: currMonstCard.race,
-                // desc: currMonstCard.desc,
                 atk: currMonstCard.atk,
                 def: currMonstCard.def,
                 cardImg: currMonstCard.card_images[0].image_url,
@@ -210,25 +177,7 @@ yugioh().then(
             monsterList.push(monsterCardObj);
         })
         game1.initialTurn(monsterList);
-
-        // ====== Need to get DOM Node only after .initialTurn() changes .html to have atkBtn ====== //
-        // ====== also get's BOTH attack buttons ====== // 
-        const $atkBtn1 = $('.atkBtn1')
-        console.log($atkBtn1);
-
-        const $atkBtn2 = $('.atkBtn2')
-        console.log($atkBtn2);
-        // try .eq()method
-
-        // ====== add's click to BOTH attack buttons ===== // 
-
         $atkBtn1.click(game1.singleBattlePhase)
-        
         $atkBtn2.click(game1.singleBattlePhase)
-
-
-        // Targeting Separate Cards! 
-        // target properly the card itself 
-        // add event listener, function going to use has a PARAMETER that accepts WHICH card pointing AT 
     }
 );
